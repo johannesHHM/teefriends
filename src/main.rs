8ed7bf657e60866f979e2345368f53bb0d6fb0b7
johannes <johannes.herman@gmail.com>
2023-04-24 18:35:55 +0200

    async code working

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,14 @@
 use serverbrowse::protocol::*;
-use serverbrowse::protocol::Response::*;
-use std::time::Duration;
-
 use tokio::net::UdpSocket;
 use std::vec;
 
-async fn send_master_request(sock: &UdpSocket, addr: &str) {
-    let header_count_6: [u8; 14] = request_count();
-    let header_list_6 = request_list_6();
-    sock.connect(addr).await.ok();
-    sock.send(&header_count_6).await.ok();
-    sock.send(&header_list_6).await.ok();
-}
+mod network;
 
-async fn recieve_master_results(sock: &UdpSocket, addr_list: &mut Vec<Addr6Packed>, server_count: &mut Option<u16>) {
-    'repeat: loop {
-        let mut buf: [u8; 1400] = [0; 1400];
-        let buf_size = sock.recv(&mut buf).await.ok();
+use crate::network::*;
 
-        match buf_size {
-            Some(ref size) => {
-                let buf: &[u8] = &buf[0..*size];
-                let response = parse_response(&buf).unwrap();
-
-                match response {
-                    Count(x) => *server_count = Some(x.0),
-                    List6(x) => {
-                        for addr in x.0.iter() {
-                            addr_list.push(*addr);
-                        }
-                    },
-                    _ => (),
-                }
-            },
-            None => (),
-        }
-        //TODO remove
-        //dbg!(&addr_list.len());
-        //dbg!(&server_count);
-        match server_count {
-            Some(ref count) => {
-                if addr_list.len() == usize::from(*count) {
-                    break 'repeat
-                }
-            },
-            //TODO what if count is never received? 
-            None => (),
-        }
-    }
-}
-
-/* fn send_info6_ex_request(sock: &UdpSocket, addr: &str, challenge: u32) -> Option<usize> {
-    let header_info_6 = request_info_6_ex(challenge);
-    sock.connect(addr).ok();
-    let sent = sock.send(&header_info_6).ok();
-    return sent;
-}
-
-fn recieve_info_result(sock: &UdpSocket) -> Option<PartialServerInfo> {
-    let mut buf: [u8; 4096] = [0; 4096];
-    let buf_size = sock.recv(&mut buf).ok();
-    match buf_size {
-        Some(ref size) => {
-            let buf: &[u8] = &buf[0..*size];
-            let response = parse_response(&buf).unwrap();
-            match response {
-                Info6(x) => {
-                    dbg!("Info6 response");
-                    //Info6Response(x.0).parse()
-                    None
-                },
-                Info664(x) => {
-                    dbg!("Info6 64 response");
-                    None
-                },
-                Info6Ex(x) => {
-                    dbg!("Info6 ex response");
-                    Info6ExResponse(x.0).parse()
-                },
-                Info6ExMore(x) => {
-                    dbg!("Info6 ex more response");
-                    Info6ExMoreResponse(x.0).parse()
-                },
-                _ => {
-                    dbg!("Defaulted out");
-                    None
-                },
-            }
-        },
-        None => None,
-    }
-} */
 #[tokio::main]
 async fn main() {
-    //let timeout = Duration::from_millis(1000);
     let socket = UdpSocket::bind("0.0.0.0:0").await.expect("could not bind socket");
-    //socket.set_read_timeout(Some(timeout)).ok();
-    //socket.set_write_timeout(Some(timeout)).ok();
 
     send_master_request(&socket, "master4.teeworlds.com:8300").await;
 
@@ -104,49 +16,43 @@ async fn main() {
     let mut server_count: Option<u16> = None;
 
     recieve_master_results(&socket, &mut addr_list, &mut server_count).await;
-
+    
     println!("{:?}", server_count);
-    //TODO change to real
-    //let mut partial_server_infos: Vec<Vec<PartialServerInfo>> = vec![Vec::new(); addr_list.len()];
-    /* let mut partial_server_infos: Vec<Vec<PartialServerInfo>> = vec![Vec::new(); 100];
+
+    let mut handles = vec![];
+    let mut results: Vec<Result<Option<ServerInfo>, tokio::task::JoinError>> = vec![];
 
     let mut i = 0;
     for addr in addr_list.as_slice() {
-        println!("{}", addr.unpack());
-
-        //TODO remove
-        if i == 100 {
-            break
-        }
-        
-        send_info6_ex_request(&socket, &addr.unpack().to_string(), i);
-        let res = recieve_info_result(&socket);
-        match res {
-            Some(r) => {
-                println!("{:?}", r);
-
-                let token = usize::try_from(r.token());
-                println!("i: {}, token: {}", i, token.ok().unwrap());
-
-                match token {
-                    Ok(x) => {
-                        partial_server_infos.get_mut(x).unwrap().push(r);
-                    },
-                    Err(_) => println!("Error with token"),
-                }
-            },
-            None => println!("Result returned None"),
-        }
+        let handle = tokio::spawn(send_recieve_server(i, addr.unpack().to_string(), 1000));
+        handles.push(handle);
+        println!("{}", i);
         i += 1;
-        println!();
+        if i % 80 == 0 || usize::try_from(i).unwrap() == addr_list.len() {
+            results.append(&mut futures::future::join_all(&mut handles).await);
+            handles.clear();
+        }
     }
-    println!("Afterwords");
-    recieve_info_result(&socket);
-    recieve_info_result(&socket);
-    recieve_info_result(&socket);
-    recieve_info_result(&socket);
 
-    for (i,v) in partial_server_infos.iter().enumerate() {
-        println!("Index: {} Length: {}", i, v.len());
-    } */
+    let mut server_infos = 0;
+    let mut none_server_infos = 0;
+
+    for result in results {
+        match result {
+            Ok(x) => {
+                match x {
+                    Some(server_info) => {
+                        //println!("Map: {}", server_info.map);
+                        server_infos += 1;
+                    },
+                    None => {
+                        none_server_infos +=1;
+                    },
+                }
+            }
+            Err(_) => (),
+        }
+    }
+
+    println!("Infos: {}, None: {}", server_infos, none_server_infos);
 }
---
+++ src/network.rs
@@ -0,0 +1,114 @@
+use serverbrowse::protocol::*;
+use serverbrowse::protocol::Response::*;
+use std::time::Duration;
+use tokio::time::timeout;
+use tokio::net::UdpSocket;
+
+pub async fn send_master_request(sock: &UdpSocket, addr: &str) {
+    let header_count_6: [u8; 14] = request_count();
+    let header_list_6 = request_list_6();
+    //sock.connect(addr).await.ok();
+    sock.send_to(&header_count_6, addr).await.ok();
+    sock.send_to(&header_list_6, addr).await.ok();
+}
+
+pub async fn recieve_master_results(sock: &UdpSocket, addr_list: &mut Vec<Addr6Packed>, server_count: &mut Option<u16>) {
+    'repeat: loop {
+        let mut buf: [u8; 1400] = [0; 1400];
+        let buf_size = sock.recv(&mut buf).await.ok();
+
+        match buf_size {
+            Some(ref size) => {
+                let buf: &[u8] = &buf[0..*size];
+                let response = parse_response(&buf).unwrap();
+
+                match response {
+                    Count(x) => *server_count = Some(x.0),
+                    List6(x) => {
+                        for addr in x.0.iter() {
+                            addr_list.push(*addr);
+                        }
+                    },
+                    _ => (),
+                }
+            },
+            None => (),
+        }
+        //TODO remove
+        //dbg!(&addr_list.len());
+        //dbg!(&server_count);
+        match server_count {
+            Some(ref count) => {
+                if addr_list.len() == usize::from(*count) {
+                    break 'repeat
+                }
+            },
+            //TODO what if count is never received? 
+            None => (),
+        }
+    }
+}
+
+pub async fn send_info6_ex_request(sock: &UdpSocket, addr: &str, challenge: u32) -> Option<usize> {
+    let header_info_6 = request_info_6_ex(challenge);
+    //sock.connect(addr).await.ok();
+    let sent = sock.send_to(&header_info_6, addr).await.ok();
+    /* match sent {
+        Some(x) => println!("SENT {}", x),
+        None => println!("SENTFAIL"),
+    } */
+    return sent;
+}
+
+pub async fn recieve_info_result(sock: &UdpSocket) -> Option<ServerInfo> {
+    let mut buf: [u8; 4096] = [0; 4096];
+    let (buf_size2, _addr) = sock.recv_from(&mut buf).await.unwrap();
+    let buf_size = Some(buf_size2);
+
+    match buf_size {
+        Some(ref size) => {
+            let buf: &[u8] = &buf[0..*size];
+            let partial_server_info: Option<PartialServerInfo>;
+            let server_info: Option<ServerInfo> = loop {
+                match parse_response(&buf).unwrap() {
+                    Info6(x) => break Info6Response(x.0).parse(),
+                    Info6Ex(x) => partial_server_info = Info6ExResponse(x.0).parse(),
+                    Info6ExMore(x) => partial_server_info = Info6ExMoreResponse(x.0).parse(),
+                    _ => break None,
+                }
+                match partial_server_info {
+                    Some(mut part_info) => {
+                        match part_info.get_info() {
+                            Some(serv_info) => break Some(serv_info.clone()),
+                            None => {
+                                //TODO what to do when more data needs reading
+                                break None
+                            },
+                        }
+                    }
+                    None => break None,
+                }
+            };
+            return server_info;
+        },
+        None => None,
+    }
+}
+
+pub async fn send_recieve_server(challenge: u32, addr: String, timeout_millis: u64) -> Option<ServerInfo> {
+    //println!("START");
+    let sock = UdpSocket::bind("0.0.0.0:0").await.expect("Error");
+
+    send_info6_ex_request(&sock, &addr, challenge).await;
+
+    match timeout(Duration::from_millis(timeout_millis), recieve_info_result(&sock)).await {
+        Ok(r) => {
+            //println!("DONE");
+            return r
+        },
+        Err(_) => {
+            //println!("TIMEOUT");
+            return None
+        },
+    };
+}
